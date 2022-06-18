e45fb50426b5b10c2f7ef55176beeb5c998d8e1f
Roman Brunner <roman.brunner94@gmail.com>
2022-06-18 13:26:56 +0000

    basic hello world

--- materials/using-gem5/02-stdlib/hello-world.py
+++ materials/using-gem5/02-stdlib/hello-world.py
@@ -6,3 +6,22 @@ from gem5.components.processors.cpu_types import CPUTypes
 from gem5.resources.resource import Resource
 from gem5.simulate.simulator import Simulator
 
+# obtain components:
+cache_hierarchy = NoCache() # Processor connects directly to memory
+memory = SingleChannelDDR3_1600("16GiB")
+processor = SimpleProcessor(cpu_type=CPUTypes.ATOMIC, num_cores=1)
+
+# Add components to the board
+# Simple board is enough for this case but might needs more
+board = SimpleBoard(
+    clk_freq="3GHz",
+    processor=processor,
+    memory=memory,
+    cache_hierarchy=cache_hierarchy,
+)
+
+binary = Resource("x86-hello64-static")
+board.set_se_binary_workload(binary)
+
+sim = Simulator(board=board)
+sim.run()
--- materials/using-gem5/02-stdlib/obtaining-resources.py
+++ materials/using-gem5/02-stdlib/obtaining-resources.py
@@ -1,5 +1,7 @@
 from gem5.resources.resource import Resource
 
+# You can implement a custom resource to specify where to download from if you have it e.g. in a repo
 resource = Resource("riscv-disk-img")
 
 print(f"The resource is available at {resource.get_local_path()}")
+
